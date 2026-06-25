02a3a73d23cbce579da84174ec66e8a77b62f408
waitimconfused <elliott_3@loonielane.ca>
2026-06-25 00:15:31 -0400

    Updated custom/playerctl module to show formatted errors, with a 30sec timeout between each refresh

--- polybar/script/playerctl.sh
+++ polybar/script/playerctl.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-status="$(playerctl status --no-messages)"
+exit_message=$( playerctl status --no-messages 2>&1)
+
+colour_background="#112717"
+colour_foreground="#AAE8B6"
+colour_primary="#4E7B31"
+colour_alert="#BB334C"
+colour_disabled="#2E462B"
+
+if [[ $exit_message != "" ]]; then
+
+	case $exit_message in
+		"Could not connect to players: Unknown or unsupported transport “disabled” for address “disabled:”")
+			exit_message="Unknown or unsupported transport"
+			;;
+	esac
+
+	for ((i = 30; i >= 0 ; i-- )); do
+		echo "%{F$colour_alert}$exit_message%{F-} ($i sec)"
+		
+		if (( i != 0 )); then sleep 1; fi
+	done
+	exit
+fi
 
 show() {
 	status="$(playerctl status --no-messages)"
@@ -27,7 +49,7 @@ show() {
 	fi
 
 
-	icon="%{F#4E7B31}󰎇%{F-}"
+	icon="%{F$colour_primary}󰎇%{F-}"
 	echo "$icon %{A:playerctl previous:}󰒮%{A} $playbutton %{A:playerctl next:}󰒭%{A} $info"
 
 }
